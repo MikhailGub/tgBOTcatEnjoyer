3fe1220dbd162dc200ed3e72b048645d0c4484fa
MikhailGub <mgubankov@yandex.ru>
2024-02-10 15:55:35 +0500

    message

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const TelegramApi = require('node-telegram-bot-api');
 
 const token = '6572216730:AAEZRw-X3IV6d75J2AgUhlx229nPKtsuEG8';
-const {gameOptions, againOptions} = require('./options');
+const {statListenerOptions, againOptions} = require('./options');
 const bot = new TelegramApi(token, {polling: true});
 
 const chats = {};
 
 
 
-const startGame = async (chatId) => {
-    await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен её отгадать`);
-    const randomNumber = Math.floor(Math.random() * 10);
-    chats[chatId] = randomNumber;
-    await bot.sendMessage(chatId, `Отгадывайте число, пожалуйста!`, gameOptions);
+const startStatListener = async (chatId) => {
+    await bot.sendMessage(chatId, `Есть 2 стула...`, statListenerOptions);
 }
 
 const start = () => {
     bot.setMyCommands([
         {command: '/start', description: 'Начальное приветствие'},
         {command: '/info', description: 'Получить информацию о пользователе'},
-        {command: '/game', description: 'Получить информацию о пользователе'},
+        {command: '/stat', description: 'Стата'},
     ])
     
     bot.on('message', async msg => {
@@ -28,14 +25,14 @@ const start = () => {
         
         if (text === '/start') {
             await bot.sendSticker(chatId, 'https://chpic.su/_data/stickers/k/kishku/kishku_012.webp?v=1706960706')
-            return bot.sendMessage(chatId, 'Добро пожаловать в телеграм бот любителя котиков')
+            return bot.sendMessage(chatId, 'Добро пожаловать в телеграм бот с какой-то статой')
         }
     
         if (text === '/info') {
             return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`)
         }
-        if (text === '/game') {
-            return startGame(chatId)
+        if (text === '/stat') {
+            return startStatListener(chatId)
         }
         return bot.sendMessage(chatId, `Я тебя не понимаю, попробуй ещё раз!`)
     })
@@ -44,12 +41,12 @@ const start = () => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
         if (data === '/again') {
-            return startGame(chatId)
+            return startStatListener(chatId)
         }
-        if (data === chats[chatId]) {
-            return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]}`, againOptions)
+        if (data === 'Win') {
+            return bot.sendMessage(chatId, `Следующий пункт`, againOptions)
         } else {
-            return bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал цифру ${data}`, againOptions)
+            return bot.sendMessage(chatId, `Проёбано`, againOptions)
         }
     })  
 }
